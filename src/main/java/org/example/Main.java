e8d3bea7f914bfcf206049e80ca2dcdf2d11c1c8
gobird <monaka@example.com>
2024-12-29 22:38:08 +0900

    first-commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,23 @@
+package org.example;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+
+
+  public static void main(String[] args) {
+
+    String fullName = "あっきー";
+
+    String yearsOld = "歳";
+
+    int age = 31;
+
+    System.out.println(fullName + "は," + age + yearsOld + "です。");
+
+
+  }
+
+
+}
+
