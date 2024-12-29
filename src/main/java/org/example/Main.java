67e72ebe1d1ef20f0e1dafa48d61759c888d013b
gobird <monaka@example.com>
2024-12-29 22:44:52 +0900

    second-commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,13 +5,18 @@ package org.example;
 public class Main {
 
 
+  /**
+   * 学生の名前と年齢を表示する。
+   *
+   * @param args
+   */
   public static void main(String[] args) {
 
-    String fullName = "あっきー";
+    String fullName = "あっきー"; //学生の名前
 
     String yearsOld = "歳";
 
-    int age = 31;
+    int age = 31; //年齢
 
     System.out.println(fullName + "は," + age + yearsOld + "です。");
 
